f4237d9e91b13b34acf78deb67d6a525d62ca211
Matchyagear <matchyagear@gmail.com>
2025-08-17 16:08:29 -0400

    Change Shadow's Picks to use MiniStockCard format

--- frontend/src/HomeScreen.js
+++ frontend/src/HomeScreen.js
@@ -862,9 +862,9 @@ const HomeScreen = ({ onNewWatchlist, watchlists, onDeleteWatchlist, news, newsL
                 </div>
               </div>
             ) : (
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+              <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4">
                 {featuredStocks.map((stock) => (
-                  <StockCard
+                  <MiniStockCard
                     key={stock.ticker}
                     stock={stock}
                     onClick={handleMiniStockClick}
